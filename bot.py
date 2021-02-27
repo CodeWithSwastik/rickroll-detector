71d7d34c4bf28e3c1a7501c5d30fcd9f69c4f263
Swas.py <61446939+CodeWithSwastik@users.noreply.github.com>
2021-02-27 10:20:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from rickroll_detector import find_rickroll
+from async_rickroll_detector import RickRollDetector
 
 BOT_TOKEN = ""
 RICKROLL_FOUND_MESSAGE = "⚠️Rickroll Alert⚠️"
@@ -8,10 +8,15 @@ RICKROLL_FOUND_MESSAGE = "⚠️Rickroll Alert⚠️"
 bot = commands.Bot(command_prefix = ">", intents = discord.Intents.default())
 
 @bot.event
+async def on_ready():
+    global detector
+    detector = RickRollDetector()
+
+@bot.event
 async def on_message(msg):
     for i in msg.content.split(" "):
         i = i.replace("<","").replace(">", "") #Removes <> that could be used to hide embeds
-        if "https://" in i and find_rickroll(i):
+        if "https://" in i and await detector.find(i):
             await msg.reply(RICKROLL_FOUND_MESSAGE)
             break
 
