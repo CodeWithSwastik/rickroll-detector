c1d4996e750c2394be56d3770771d01004d99ca5
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2021-02-26 22:43:12 -0600

    fix

--- async_rickroll_detector.py
+++ async_rickroll_detector.py
@@ -3,7 +3,7 @@ import re
   
 class RickRollDetector(aiohttp.ClientSession):
   
-  async def find(self, url):
-      source = str((await self.get(url)).content).lower()
-      phrases = ["rickroll","rick roll","rick astley","never gonna give you up"]
-      return bool(re.findall("|".join(phrases), source, re.MULTILINE))
+    async def find(self, url):
+        source = str(await (await super().get(url)).content.read()).lower()
+        phrases = ["rickroll","rick roll","rick astley","never gonna give you up"]
+        return bool(re.findall('|'.join(phrases), source, re.MULTILINE))f
