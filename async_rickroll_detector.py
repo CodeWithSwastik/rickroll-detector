a77a9bf0ee04974202a77667e0f46128bb0ea2cb
Swas.py <61446939+CodeWithSwastik@users.noreply.github.com>
2021-02-27 10:16:45 +0530

    Update async_rickroll_detector.py

--- async_rickroll_detector.py
+++ async_rickroll_detector.py
@@ -2,8 +2,8 @@ import aiohttp
 import re
   
 class RickRollDetector(aiohttp.ClientSession):
-  
+
     async def find(self, url):
         source = str(await (await super().get(url)).content.read()).lower()
         phrases = ["rickroll","rick roll","rick astley","never gonna give you up"]
-        return bool(re.findall('|'.join(phrases), source, re.MULTILINE))f
+        return bool(re.findall('|'.join(phrases), source, re.MULTILINE))
